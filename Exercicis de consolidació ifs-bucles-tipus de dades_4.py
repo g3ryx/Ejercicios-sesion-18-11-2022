2ee199d672d2360121cfae4e275b0ea29f0ae54d
g3ryx <117978978+g3ryx@users.noreply.github.com>
2022-11-23 08:28:09 +0100

    Create Exercicis de consolidació ifs-bucles-tipus de dades_4.py

---
+++ Exercicis de consolidació ifs-bucles-tipus de dades_4.py
@@ -0,0 +1,21 @@
+num1 = float(input('Introduzca un operador: '))
+num2 = float(input('Introduzca un operador: '))
+sign = str(input('Introduzca una operando: '))
+
+if sign == "+":
+    print(num1+num2)
+
+if sign == "-":
+    print(num1-num2)
+
+if sign == "*":
+    print(num1*num2)
+
+if sign == "/":
+    print(num1/num2)
+
+if sign == "//":
+    print(num1//num2)
+
+if sign == "%":
+    print(num1%num2)
